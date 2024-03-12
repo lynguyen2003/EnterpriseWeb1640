d264aa9a71562d0689be5307e2a8d976107b7850
lynguyen <nly36524@gmail.com>
2024-03-12 20:42:55 +0700

    feet(Contributions): add Contributions API

--- API/DataServices/Interfaces/IContributionsRepository.cs
+++ API/DataServices/Interfaces/IContributionsRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,8 @@ using System.Threading.Tasks;
 
 namespace DataServices.Interfaces
 {
-    public interface IUnitOfWorks
+    public interface IContributionsRepository : IGenericRepository<Contributions>
     {
-        IContributionsRepository Contributions { get; }
-        Task<bool> CompleteAsync();
-        
+
     }
 }
---
+++ API/Models/DTO/Request/ContributionsRequestUpdateDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DTO.Request
+{
+    public class ContributionsRequestUpdateDTO
+    {
+        [Required(ErrorMessage = "Id is required")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Title must be between 5 and 100 characters")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        [StringLength(500, MinimumLength = 10, ErrorMessage = "Description must be between 10 and 500 characters")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "FilePath is required")]
+        [Url(ErrorMessage = "FilePath must be a valid URL")]
+        public string FilePath { get; set; }
+
+        [Required(ErrorMessage = "ClosureDatesId is required")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "ClosureDatesId must be a numeric value")]
+        public string ClosureDatesId { get; set; }
+    }
+}
